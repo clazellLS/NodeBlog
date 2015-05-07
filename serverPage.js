8498607337883a13fa4ad23bb26177f63ed8a062
Cameron Lazell <cameron.lazell@lisasoft.com>
2015-05-07 15:46:16 +1000

    Simple blog framework

--- serverPage.js
+++ serverPage.js
@@ -1,7 +1,48 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
+  var filePath = false;
+  console.log("request", request.url);
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+}).listen(process.env.PORT || 3000);
+
+
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
   response.end();
-}).listen(3000);
+}
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+  console.log("absPath", absPath);
+    if (exists) {
+	console.log("EXISTS");
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+	console.log("ERROOR");
+      send404(response);
+    }
+  });
+}
